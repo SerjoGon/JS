58ab91f9481248b8b37ce485478ff2f42c2d9f54
SergeyGoncharov <mr.serhioru@mail.ru>
2023-06-09 11:09:25 +0300

    123

---
+++ lessons/new/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  
+});
+function redClick(){
+let redClickTarget = document.querySelector("body");
+redClickTarget.style.backgroundColor = 'red';
+}
+function yellowClick(){
+    let yellowClickTarget = document.querySelector("body");
+    yellowClickTarget.style.backgroundColor = 'yellow';
+}
