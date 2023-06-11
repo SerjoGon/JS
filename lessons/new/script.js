52c120a15435698504d0fd835dc07e89c9b0d0c4
SergeyGoncharov <mr.serhioru@mail.ru>
2023-06-11 20:43:05 +0300

    ok

--- lessons/new/script.js
+++ lessons/new/script.js
@@ -1,6 +1,9 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   
 });
+
+
+// Первое задание
 function redClick(){
 let redClickTarget = document.querySelector("body");
 redClickTarget.style.backgroundColor = 'red';
@@ -9,3 +12,25 @@ function yellowClick(){
     let yellowClickTarget = document.querySelector("body");
     yellowClickTarget.style.backgroundColor = 'yellow';
 }
+
+
+
+//Третье задание не полностью
+button_Red = document.querySelector("button#btn-RED");
+button_yellow = document.querySelector("button#btn-YELLOW");
+button_green = document.querySelector("button#btn-GREEN");
+red = document.querySelector(".redLight");
+yellow = document.querySelector(".yellowLight");
+green = document.querySelector(".greenLight");
+function redClick(){
+    red.style.background = "rgb(165, 0, 0)";
+}
+function yellowClick(){
+    yellow.style.background = "rgb(230, 230, 0)";
+}
+function greenClick(){
+    green.style.background = "rgb(0, 222, 0)";
+}
+button_Red.addEventListener("click",redClick);
+button_yellow.addEventListener("click",yellowClick);
+button_green.addEventListener("click",greenClick);
